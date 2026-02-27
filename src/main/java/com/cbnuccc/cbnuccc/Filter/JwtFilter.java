c82449c53cfe2cb72d912f123cf684bf52e49c5e
choidh88 <dev.cdh88@gmail.com>
2026-02-27 22:33:50 +0900

    Reomve ip log.

--- src/main/java/com/cbnuccc/cbnuccc/Filter/JwtFilter.java
+++ src/main/java/com/cbnuccc/cbnuccc/Filter/JwtFilter.java
@@ -90,7 +90,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         // print a entering log
         MDC.put("entered_user_uuid", claim.get("uuid").toString().substring(0, 8));
-        MDC.put("ip", SecurityUtil.getClientIp(request));
+        // MDC.put("ip", SecurityUtil.getClientIp(request));
         LogUtil.printBasicInfoLog(LogHeader.ENTER, (Object[]) null);
 
         // final setting to login.
